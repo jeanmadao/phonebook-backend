27c93d37fe7a0211afda387aecd167fab6635afa
Jean Huynh <huynhjeanpierre@hotmail.com>
2023-07-31 19:32:16 +0200

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 let persons = [
-  { 
+  {
     "id": 1,
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": 2,
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": 3,
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": 4,
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ]
@@ -29,6 +30,7 @@ let persons = [
 const generateId = () => Math.floor(Math.random() * 100000)
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('data-sent', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data-sent'))
@@ -39,7 +41,7 @@ app.get('/api/persons', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  
+
   if (body.name && body.number) {
     const potentialPerson = persons.find(person => person.name === body.name)
     if (!potentialPerson) {
@@ -86,7 +88,7 @@ app.get('/info', (request, response) => {
   `)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
